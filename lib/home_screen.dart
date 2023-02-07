52334a3e313d89511e6a34a0a44c8da224a1b898
ylose <heungeob1003@gmail.com>
2023-02-07 12:25:36 +0900

    _CoupleImage 위젯에 커플 이미지 추가하기

--- lib/home_screen.dart
+++ lib/home_screen.dart
@@ -40,6 +40,14 @@ class _CoupleImage extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Text('Couple Image Widget');
+    return Center(
+      child:
+      Image.asset(
+        'asset/img/middle_image.png',
+
+        // 화면의 반만큼 높이 구현
+        height: MediaQuery.of(context).size.height/2,
+    )
+    );
   }
 }
