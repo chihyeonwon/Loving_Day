c034aca85d0d790693b3b312ed883143292e887c
ylose <heungeob1003@gmail.com>
2023-02-07 12:06:45 +0900

    _DDay와 _CoupleImage 위젯 배치

--- lib/home_screen.dart
+++ lib/home_screen.dart
@@ -6,7 +6,21 @@ class HomeScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Text('Home Screen'),
+      body: SafeArea( // 시스템 UI 피해서 UI 그리기
+        top: true,
+        bottom:false,
+        child:Column(
+          // 위아래 끝에 위젯 배치
+          mainAxisAlignment: MainAxisAlignment.spaceBetween,
+
+          // 반대축 최대 크기로 늘리기
+          crossAxisAlignment: CrossAxisAlignment.stretch,
+          children:[
+            _DDay(),
+            _CoupleImage(),
+          ]
+        )
+      )
     );
   }
 }
